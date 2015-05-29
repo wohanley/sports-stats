330dd6004977595dd1692127b81223842ade1ad0
William O'Hanley <liam@wohanley.com>
2015-05-29 00:36:41 -0300

    move modifiers to their own file

--- src/sports/grammar/modifiers.clj
+++ src/sports/grammar/modifiers.clj
@@ -1,7 +1,10 @@
-(ns sports.grammar
+(ns sports.grammar.modifiers
   (:require [robots.grammar.generative :refer [terminate]]
             [clojure.data.generators :refer [weighted]]))
 
+;;; modifiers
+
+;; venue
 
 (defn in-location []
   (rand-nth ["Halifax" "Charlottetown" "Vancouver" "Toronto" "Montreal"
@@ -13,8 +16,12 @@
   (rand-nth ["a hillside" "a mountain" "a barge" "frozen lakes"
              "top of tall buildings" "loam" "a peat bog"]))
 
-(defn start []
+(defn venue []
   (weighted {"at home" 1
              "away" 1
              ["in " in-location] 2
              ["on " on-location] 2}))
+
+;; audience
+
+(defn audience )
